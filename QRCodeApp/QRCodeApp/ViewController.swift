80988e25da9a79c8b667e5ecc261a7294762a27c
leandro <leandro.a.estrada@gmail.com>
2020-12-10 12:07:32 -0300

    Starting the project

---
+++ QRCodeApp/QRCodeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCodeApp
+//
+//  Created by leandro de araujo estrada on 10/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
